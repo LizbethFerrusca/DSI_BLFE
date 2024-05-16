aeb8783b3e349815a8ce268d560dc5795b4306a7
LizbethFerrusca <bferrusca28@alumnos.uaq.mx>
2024-05-15 20:00:03 -0600

    peticion post

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+
+const db = require("./config/database")
 const express = require ("express");
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", (req, res, next) => {
-    console.log(req);
-    res.send("Hello world!");
+    res.render("index");
 });
 
-app.get("/pokemon", (req, res, next) => {
-    res.json({ name: "Pikachu", type: "Electric" });
+app.get("/pokemon", async (req, res, next) => {
+    
+    const pokemon = await db.query("SELECT * FROM pokemon;");
+    console.log(pokemon);
+    res.json(pokemon);
   });
+
+app.post("/pokemon", (req, res, next) =>{
+    console.log(req.body);
+    res.send("¡Información guardada!");
+})
 
 app.listen (3000, () =>{
     console.log("Server is running...");
